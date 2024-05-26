2750d1db0c3b3f3e4716bf82059acecb7893c395
Arpit Bhat <arpitbhat05@gmail.com>
2024-05-26 14:36:46 -0400

    Now working with mood

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("This is my first Python script!!")
+import sys
+
+def hello(name, mood):
+    print("Hello",name)
+    print(f"Your mood is {mood} today!")
+
+name = sys.argv[1]
+mood = sys.argv[2]
+hello(name, mood)

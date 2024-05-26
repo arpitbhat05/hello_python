35abc574e887d54a74bc1477e623c3d1ec2d462e
Arpit Bhat <arpitbhat05@gmail.com>
2024-05-26 14:42:38 -0400

    Custom msg based on mood

--- hello.py
+++ hello.py
@@ -3,6 +3,11 @@ import sys
 def hello(name, mood):
     print("Hello",name)
     print(f"Your mood is {mood} today!")
+    if mood == "Great":
+        print("It cant get any better!")
+    else:
+        print("Do you want me to sing your fav song!")
+
 
 name = sys.argv[1]
 mood = sys.argv[2]
--- hello2.py
+++ hello2.py
@@ -1,1 +1,2 @@
 print("This is my super complex script!")
+print("dont you like it!!")
